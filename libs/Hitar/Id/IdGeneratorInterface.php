cf4b99344164656483ca3bbeaa5c2302c202359b
kasonyang <i@kasonyang.com>
2014-07-09 13:35:43 +0800

    接口修正

--- libs/Hitar/Id/IdGeneratorInterface.php
+++ libs/Hitar/Id/IdGeneratorInterface.php
@@ -9,11 +9,16 @@
 namespace Hitar\Id;
 
 interface IdGeneratorInterface {
+    
+    function __construct($params = []);
+    
     /**
      * 
      * @param \Doctrine\DBAL\Connection $connection
      * @return mixed 返回ID值
      */
     function generate($connection);
+    
+    function isGenerateAfterInsert();
     
 }
